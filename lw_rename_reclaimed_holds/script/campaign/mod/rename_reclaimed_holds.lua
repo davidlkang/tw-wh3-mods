008f5847599c8412f02a8cc315d4a13c8cc62523
davidlkang <david.kang@outlook.de>
2024-03-11 17:59:41 +0100

    add toggle for it to work for the AI

--- lw_rename_reclaimed_holds/script/campaign/mod/rename_reclaimed_holds.lua
+++ lw_rename_reclaimed_holds/script/campaign/mod/rename_reclaimed_holds.lua
@@ -1,6 +1,7 @@
 local run_every_start_up = false
 local dwarf_culture = "wh_main_sc_dwf_dwarfs"
 local dwarf_holds
+local run_for_ai = false
 
 
 local function check_campaign_compatibility()
@@ -28,10 +29,19 @@ end
 local function check_if_dwarf_player(faction)
     local is_player = faction:is_human()
     local is_dwarf_culture = faction:subculture() == dwarf_culture
+    local should_run
 
-    if is_player and is_dwarf_culture then
-        return true
+    if is_dwarf_culture then
+        if run_for_ai then
+            should_run = true
+        elseif is_player then
+            should_run = true
+        end
+    else
+        should_run = false
     end
+
+    return should_run
 end
 
 
@@ -44,8 +54,8 @@ local function rename_new_hold(region)
 end
 
 
-local function rename_owned_holds(player_faction)
-    local region_list = player_faction:region_list()
+local function rename_owned_holds(faction)
+    local region_list = faction:region_list()
     local number_of_regions = region_list:num_items()
 
     for i = 0, number_of_regions - 1 do
@@ -55,14 +65,22 @@ local function rename_owned_holds(player_faction)
             cm:change_custom_settlement_name(region_list:item_at(i):settlement(), new_region_name)
         end
     end
+end
 
-    cm:set_saved_value("has_renamed_owned_holds", true)
+
+local function rename_owned_holds_all(player_faction)
+    local dwarf_factions = player_faction:factions_of_same_subculture()
+    local number_of_dwarf_factions = dwarf_factions:num_items()
+
+    for i = 0, number_of_dwarf_factions - 1 do
+        rename_owned_holds(dwarf_factions:item_at(i))
+    end
 end
 
 
 local function add_dwarf_hold_listener()
     core:add_listener(
-        "DwarfPlayerGetsRegion",
+        "DwarfFactionGetsRegion",
         "RegionFactionChangeEvent",
         function(context)
             return check_if_dwarf_player(context:region():owning_faction())
@@ -87,6 +105,10 @@ local function add_mct_finalized_listener()
             if mod:get_option_by_key("lw_rename_on_every_start_up"):get_finalized_setting() then
                 rename_owned_holds(cm:get_local_faction())
             end
+
+            if mod:get_option_by_key("lw_run_for_ai"):get_finalized_setting() then
+                rename_owned_holds_all(cm:get_local_faction())
+            end
         end,
         true
     )
@@ -99,11 +121,17 @@ local function init()
 
     add_mct_finalized_listener()
 
-    if player_faction:subculture() == dwarf_culture then
+    if player_faction:subculture() == dwarf_culture or run_for_ai then
         add_dwarf_hold_listener()
 
         if has_not_renamed_owned_holds or run_every_start_up then
-            rename_owned_holds(player_faction)
+            if run_for_ai then
+                rename_owned_holds_all(player_faction)
+            else
+                rename_owned_holds(player_faction)
+            end
+
+           cm:set_saved_value("has_renamed_owned_holds", true)
         end
     end
 end
@@ -118,6 +146,7 @@ core:add_listener(
         local mod = mct:get_mod_by_key("lw_rename_reclaimed_holds")
 
         run_every_start_up = mod:get_option_by_key("lw_rename_on_every_start_up"):get_finalized_setting()
+        run_for_ai = mod:get_option_by_key("lw_run_for_ai"):get_finalized_setting()
     end,
     true
 )
--- lw_rename_reclaimed_holds/script/mct/settings/rename_reclaimed_holds.lua
+++ lw_rename_reclaimed_holds/script/mct/settings/rename_reclaimed_holds.lua
@@ -16,6 +16,15 @@ local run_every_start_up_settings = {
         "will run the script to rename owned settlements just once per campaign."
 }
 
+local run_for_ai_settings = {
+    name = "run_for_ai",
+    default_value = false,
+    text = "Run for AI dwarfs",
+    tooltip_text = "When this is enabled, the script will also run for AI dwarfs." ..
+        "I would not recommend to also have the run-every-startup setting active." ..
+        "It is not that something breaks, but your game might load a bit longer if you do."
+}
+
 
 local mct = get_mct()
 local mct_mod = mct:register_mod("lw_rename_reclaimed_holds")
@@ -24,8 +33,16 @@ mct_mod:set_title(mod_settings.title)
 mct_mod:set_author(mod_settings.author)
 mct_mod:set_description(mod_settings.descrpition)
 
+
 local run_every_start_up = mct_mod:add_new_option("lw_rename_on_every_start_up", "checkbox")
 
 run_every_start_up:set_default_value(run_every_start_up_settings.default_value)
 run_every_start_up:set_text(run_every_start_up_settings.text)
 run_every_start_up:set_tooltip_text(run_every_start_up_settings.tooltip_text)
+
+
+local run_for_ai = mct_mod:add_new_option("lw_run_for_ai", "checkbox")
+
+run_for_ai:set_default_value(run_for_ai_settings.default_value)
+run_for_ai:set_text(run_for_ai_settings.text)
+run_for_ai:set_tooltip_text(run_for_ai_settings.tooltip_text)
